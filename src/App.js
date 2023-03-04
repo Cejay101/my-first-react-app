335dde1b413d5743c87692f2e99f3a7b10448a0f
cejay101 <clintonjoyfimie@gmail.com>
2023-03-04 11:51:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { faArrowRight, faCopy } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faGithub,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="password-generator">
+      <h2 className="header">Password Generator</h2>
+      <div className="copy-password">
+        <h3>password</h3>
+        <FontAwesomeIcon className="icon" icon={faCopy} />
+      </div>
+      <div className="generator">
+        <div className="character-length">
+          <p>Character Length</p>
+          <p className="length">10</p>
+        </div>
+        <input type="range" className="slider" />
+        <br />
+        <label>
+          <input className="check" type="checkbox" />
+          <span>Include Uppercase Letter</span>
+        </label>
+        <br />
+        <label>
+          <input className="check" type="checkbox" />
+          <span>Include Lowercase Letter</span>
+        </label>
+        <br />
+        <label>
+          <input className="check" type="checkbox" />
+          <span>Include Numbers</span>
+        </label>
+        <br />
+        <label>
+          <input className="check" type="checkbox" />
+          <span>Include symbols</span>
+        </label>
+        <br />
+        <div className="strength">
+          <p>STRENGTH</p>
+          <p className="level">Medium</p>
+          <div className="bars">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+        </div>
+        <button>
+          <p>GENERATE </p>{" "}
+          <FontAwesomeIcon
+            className="icon"
+            icon={faArrowRight}
+            style={{ color: "black" }}
+          />
+        </button>
+      </div>
+      <p className="footer-text">
+        Created by Clinton Joy - Frontend Mentor challenge{" "}
+      </p>
+      <div className="socials">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Cejay101"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FontAwesomeIcon className="icon" icon={faGithub} />
         </a>
-      </header>
+        <a
+          href="https://www.twitter.com/clintonjoy10"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon className="icon" icon={faTwitter} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/clinton-joy-538804244/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon className="icon" icon={faLinkedinIn} />
+        </a>
+      </div>
     </div>
   );
 }
-
 export default App;
